5d5c88a5574e390f8e8ec7497361c80cc19dda94
vishmal <vishmalwkc@gmail.com>
2016-09-22 19:07:03 +0530

    day3.1 changes

---
+++ features/bootstrap/myfeature.feature
@@ -0,0 +1,11 @@
+Feature: Todo list
+  In order to list items
+  As a user
+  I need to be able to add items I should do next in to my list
+
+  #Rules:
+  #- VAT is 20%
+  #- Delivery for basket under £10 is £3
+  #- Delivery for basket over £10 is £2
+Scenario: Buying a single product under £10
+  Given there is a "Sith Lord Lightsaber", which costs £5pwd
--- var/cache/dev/twig/d7/d73435d49f5d2ae3f41ad3969223948ce5066208f2531cafb77741a400b615fe.php
+++ var/cache/dev/twig/d7/d73435d49f5d2ae3f41ad3969223948ce5066208f2531cafb77741a400b615fe.php
@@ -22,20 +22,20 @@ class __TwigTemplate_196b1320e74fa340561c50dac479179cc3019b84fcdfe836fd64ede4444
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_ff78602d9f3469b6a0e1be9b95ff48f8748ef22158b9c3238aa93336f28d473f = $this->env->getExtension("native_profiler");
-        $__internal_ff78602d9f3469b6a0e1be9b95ff48f8748ef22158b9c3238aa93336f28d473f->enter($__internal_ff78602d9f3469b6a0e1be9b95ff48f8748ef22158b9c3238aa93336f28d473f_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "todo/home.html.twig"));
+        $__internal_3fc8c04b112228b2c7aa584c559fd7d7d3c3939eda67b007eea6b7826995fc23 = $this->env->getExtension("native_profiler");
+        $__internal_3fc8c04b112228b2c7aa584c559fd7d7d3c3939eda67b007eea6b7826995fc23->enter($__internal_3fc8c04b112228b2c7aa584c559fd7d7d3c3939eda67b007eea6b7826995fc23_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "todo/home.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_ff78602d9f3469b6a0e1be9b95ff48f8748ef22158b9c3238aa93336f28d473f->leave($__internal_ff78602d9f3469b6a0e1be9b95ff48f8748ef22158b9c3238aa93336f28d473f_prof);
+        $__internal_3fc8c04b112228b2c7aa584c559fd7d7d3c3939eda67b007eea6b7826995fc23->leave($__internal_3fc8c04b112228b2c7aa584c559fd7d7d3c3939eda67b007eea6b7826995fc23_prof);
 
     }
 
     // line 3
     public function block_body($context, array $blocks = array())
     {
-        $__internal_a0d55f86a63d91782678c015e1454f6c62263ee6a5fd2d052afee78a1ffb524f = $this->env->getExtension("native_profiler");
-        $__internal_a0d55f86a63d91782678c015e1454f6c62263ee6a5fd2d052afee78a1ffb524f->enter($__internal_a0d55f86a63d91782678c015e1454f6c62263ee6a5fd2d052afee78a1ffb524f_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_b4241f146eebc837a93690cdaf74c8d871ad5ae95ca63184fc777b0dd372c9d7 = $this->env->getExtension("native_profiler");
+        $__internal_b4241f146eebc837a93690cdaf74c8d871ad5ae95ca63184fc777b0dd372c9d7->enter($__internal_b4241f146eebc837a93690cdaf74c8d871ad5ae95ca63184fc777b0dd372c9d7_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 4
         echo "    <div class=\"panel panel-default\" ng-app=\"myApp\" ng-controller=\"myCtrl\">
@@ -87,6 +87,10 @@ class __TwigTemplate_196b1320e74fa340561c50dac479179cc3019b84fcdfe836fd64ede4444
                     <td ng-show=\"item.priority == 1\">High</td>
                     <td ng-show=\"item.priority == 2\">Normal</td>
                     <td ng-show=\"item.priority == 3\">Low</td>
+                    <td>";
+        // line 44
+        echo "{{ item.created }}";
+        echo "</td>
                     <td><input type=\"button\" ng-click=\"delete(item.id)\" value=\"Delete\"></td>
                 </tr>
 
@@ -96,23 +100,23 @@ class __TwigTemplate_196b1320e74fa340561c50dac479179cc3019b84fcdfe836fd64ede4444
 
 ";
         
-        $__internal_a0d55f86a63d91782678c015e1454f6c62263ee6a5fd2d052afee78a1ffb524f->leave($__internal_a0d55f86a63d91782678c015e1454f6c62263ee6a5fd2d052afee78a1ffb524f_prof);
+        $__internal_b4241f146eebc837a93690cdaf74c8d871ad5ae95ca63184fc777b0dd372c9d7->leave($__internal_b4241f146eebc837a93690cdaf74c8d871ad5ae95ca63184fc777b0dd372c9d7_prof);
 
     }
 
-    // line 53
+    // line 54
     public function block_javascripts($context, array $blocks = array())
     {
-        $__internal_ea7ae136f52f15c272f2694c25776c2a4c97fce35c2d3508a21d708cb410a936 = $this->env->getExtension("native_profiler");
-        $__internal_ea7ae136f52f15c272f2694c25776c2a4c97fce35c2d3508a21d708cb410a936->enter($__internal_ea7ae136f52f15c272f2694c25776c2a4c97fce35c2d3508a21d708cb410a936_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+        $__internal_2ca952ff4f42c0e6c415359d82a367df43e5b95f6d5dc14348fa416a6c16222d = $this->env->getExtension("native_profiler");
+        $__internal_2ca952ff4f42c0e6c415359d82a367df43e5b95f6d5dc14348fa416a6c16222d->enter($__internal_2ca952ff4f42c0e6c415359d82a367df43e5b95f6d5dc14348fa416a6c16222d_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
 
-        // line 54
+        // line 55
         echo "    <script src=\"";
         echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("js/home.js"), "html", null, true);
         echo "\"></script>
 ";
         
-        $__internal_ea7ae136f52f15c272f2694c25776c2a4c97fce35c2d3508a21d708cb410a936->leave($__internal_ea7ae136f52f15c272f2694c25776c2a4c97fce35c2d3508a21d708cb410a936_prof);
+        $__internal_2ca952ff4f42c0e6c415359d82a367df43e5b95f6d5dc14348fa416a6c16222d->leave($__internal_2ca952ff4f42c0e6c415359d82a367df43e5b95f6d5dc14348fa416a6c16222d_prof);
 
     }
 
@@ -128,7 +132,7 @@ class __TwigTemplate_196b1320e74fa340561c50dac479179cc3019b84fcdfe836fd64ede4444
 
     public function getDebugInfo()
     {
-        return array (  110 => 54,  104 => 53,  85 => 40,  81 => 39,  77 => 38,  41 => 4,  35 => 3,  11 => 1,);
+        return array (  114 => 55,  108 => 54,  92 => 44,  85 => 40,  81 => 39,  77 => 38,  41 => 4,  35 => 3,  11 => 1,);
     }
 }
 /* {% extends 'base.html.twig' %}*/
@@ -174,6 +178,7 @@ class __TwigTemplate_196b1320e74fa340561c50dac479179cc3019b84fcdfe836fd64ede4444
 /*                     <td ng-show="item.priority == 1">High</td>*/
 /*                     <td ng-show="item.priority == 2">Normal</td>*/
 /*                     <td ng-show="item.priority == 3">Low</td>*/
+/*                     <td>{{ '{{ item.created }}' }}</td>*/
 /*                     <td><input type="button" ng-click="delete(item.id)" value="Delete"></td>*/
 /*                 </tr>*/
 /* */
